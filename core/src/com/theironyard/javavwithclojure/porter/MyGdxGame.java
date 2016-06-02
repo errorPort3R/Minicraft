846ae6c9f6aeb649c32506de7302d65014ad0981
jeffryporter <jporter7@gmail.com>
2016-06-02 13:19:19 -0400

    Initial commit. Add movement with correct facing.

---
+++ core/src/com/theironyard/javavwithclojure/porter/MyGdxGame.java
@@ -0,0 +1,121 @@
+package com.theironyard.javavwithclojure.porter;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture tiles;
+	TextureRegion[][] grid;
+	TextureRegion down;
+	TextureRegion up;
+	TextureRegion right;
+	TextureRegion left;
+	TextureRegion stand;
+	float x, y, xv, yv;
+
+	static final int WIDTH = 16;
+	static final int HEIGHT = 16;
+	static final float MAX_VELOCITY = 100;
+	static final float DECLERATION_RATE = 0.95f;
+
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		tiles = new Texture("tiles.png");
+		grid = TextureRegion.split(tiles, WIDTH, HEIGHT);
+		down = grid[6][0];
+		up = grid[6][1];
+		stand = grid[6][2];
+		right = grid[6][3];
+		left = new TextureRegion(right);
+		left.flip(true, false);
+	}
+
+	@Override
+	public void render () {
+		move();
+
+		//time += Gdx.graphics.getDeltaTime();
+
+		TextureRegion img;
+		if (xv<0)
+		{
+			img = left;
+		}
+		else if (xv>0)
+		{
+			img = right;
+		}
+		else if (yv<0)
+		{
+			img = down;
+		}
+		else if (yv>0)
+		{
+			img = up;
+		}
+		else
+		{
+			img = stand;
+		}
+
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.8f, 0.7f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, x, y, WIDTH*3, HEIGHT*3);
+		batch.end();
+	}
+
+	public void move()
+	{
+		if (Gdx.input.isKeyPressed(Input.Keys.UP))
+		{
+			yv = MAX_VELOCITY;
+		}
+		else if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
+		{
+			yv = -MAX_VELOCITY;
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+		{
+			xv = MAX_VELOCITY;
+		}
+		else if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+		{
+			xv = -MAX_VELOCITY;
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE))
+		{
+			xv *= MAX_VELOCITY*2;
+			yv *= MAX_VELOCITY*2;
+		}
+
+
+
+		float delta = Gdx.graphics.getDeltaTime();
+		y+= yv * delta;
+		x+= xv * delta;
+		yv = decelerate(yv);
+		xv = decelerate(xv);
+
+	}
+
+	public float decelerate(float velocity)
+	{
+		velocity *= DECLERATION_RATE;
+		if (Math.abs(velocity) < 1)
+		{
+			velocity = 0;
+		}
+		return velocity;
+	}
+}
---
+++ desktop/src/com/theironyard/javavwithclojure/porter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.theironyard.javavwithclojure.porter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.theironyard.javavwithclojure.porter.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
